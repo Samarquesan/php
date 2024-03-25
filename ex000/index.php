d0b98920a2fc47bdf07f072cfe308b8bae155d6e
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-03-25 16:32:54 -0300

    Inserção do exercício 02

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "Hello world! \u{1f30E}"
+        ?>
+    </h1>
+    <p>Let´s get rid of the curse!</p>
+</body>
+</html>

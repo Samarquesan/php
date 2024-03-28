3a859072edaca8104d65129b783c50998ea771fe
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-03-27 23:40:38 -0300

    variáveis

---
+++ ex003/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis e Constantes</title>
+</head>
+<body>
+    <h1>Variaveis e constantes</h1>
+    <?php 
+        $num1 = 1;
+        $num2 =2;
+        $soma = $num1 + $num2;
+        echo "$soma"
+    ?>
+</body>
+</html>

e464a217e18abf254d636caaf86cd32e34ba82f5
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-04-01 17:32:06 -0300

    vrs

--- ex004/index.php
+++ ex004/index.php
@@ -10,7 +10,30 @@
     <h1>Entendendo as variáveis</h1>
     <h2>Váriaveis primitivas</h2>
     <?php 
-    
+     //0x = hexadecimal 0b = binário 0 = octal
+     //     $num = 010;
+     //     echo "O valor da variável é $num";
+
+    //  $v = "Samuel";
+    //  var_dump($v);
+
+    // $num = (int)3e2;//coerção força a tipagem da variável
+    // echo "O valor é $num.";
+    // var_dump($num);
+
+    // $num = (double) "950";
+    // var_dump($num);
+
+    // $vet= [10,20,30,40,50];
+    // var_dump($vet);
+
+    class Pessoa {
+        private string $nome;
+    }
+
+    $p = new Pessoa;
+    var_dump($p);
+
     ?>
 </body>
 </html>

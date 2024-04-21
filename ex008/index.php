8cc3d91afce2c70e9d5592effead4d7b7c970d78
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-04-21 19:23:02 -0300

    Operadores números pt2

---
+++ ex008/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Execício 08</title>
+</head>
+<body>
+    <?php 
+        $resultado = 5+2;
+        echo "E resultado foi: $resultado \n";
+        //var_dump($resultado);
+         $resultado = 5-2;
+         echo "E resultado foi: $resultado \n";
+         $resultado = 5*2;
+         echo "E resultado foi: $resultado \n";
+         $resultado = 5/2;
+         echo "E resultado foi: $resultado \n";
+         $resultado = 5%2;
+         echo "E resultado foi: $resultado \n";
+         $resultado = 5**2;
+         echo "E resultado foi: $resultado \n";
+    ?>
+</body>
+</html>

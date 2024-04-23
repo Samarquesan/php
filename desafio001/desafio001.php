8bc0c72c7798a28fec9c793e1d7ead5c2e181f25
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-04-23 00:57:00 -0300

    desafio01

---
+++ desafio001/desafio001.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 01</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div id="mini_tela" class="mini_tela">
+        <h1>Resultado Final</h1>
+        <input type="number" name="" id="num_escolhido" class="num_escolhido">
+        <div id="num_escolhido" class="num_escolhido">O número escolhido é: <input type="number" name="" id="num_escolhido" class="num_escolhido"></div>
+    </div>
+</body>
+</html>

6a4a62920355f22a28c6a39c14d859dba823afc2
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-04-24 01:02:04 -0300

    desafio01

--- desafio001/desafio001.php
+++ desafio001/desafio001.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 01</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styledesafio01.css">
 </head>
 <body>
-    
+    <h1>Informe um número</h1>
     <div id="mini_tela" class="mini_tela">
-        <h1>Resultado Final</h1>
+        <div id="numero" class="numero">O número:
+        </div>
         <input type="number" name="" id="num_escolhido" class="num_escolhido">
-        <div id="num_escolhido" class="num_escolhido">O número escolhido é: <input type="number" name="" id="num_escolhido" class="num_escolhido"></div>
+        <input type="button" value="Calcular">
     </div>
 </body>
 </html>

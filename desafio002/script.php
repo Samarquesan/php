255ceac6c254d0595f0bc1f7cfde115cf2323aaf
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-05-01 17:23:01 -0300

    desafio 02

---
+++ desafio002/script.php
@@ -0,0 +1,9 @@
+<?php 
+    $min = 0;
+    $max= 100;
+    
+    $num = mt_rand($min, $max);
+
+    echo "<p>Gerando um número aleatório... </br>O número soretado foi: <strong>$num</strong></p>";
+
+?>

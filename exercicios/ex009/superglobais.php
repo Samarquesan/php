a6c15c6bb0ccc15af26100adf8b45e426d90d1b5
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-06-27 00:18:04 -0300

    fim do exercicio

--- exercicios/ex009/superglobais.php
+++ exercicios/ex009/superglobais.php
@@ -34,6 +34,8 @@
 
                 echo "<h1>Superglobal SERVER</h1>";
                 var_dump($_SERVER);
+                echo "<h1>Superglobal GLOBALS</h1>";
+                var_dump($GLOBALS);
             ?>
         </pre>
     </main>
